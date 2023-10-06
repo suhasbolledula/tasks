145be45d3cfa81cb4d26ff8f56139cbf2f4eca4c
Suhas Bolledula <suhasb@udel.edu>
2023-10-06 16:03:09 -0400

    Task 10 Completed

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,41 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptsRequested, setAttemptsRequested] = useState<number>(0);
+
+    function updateAttemptsLeft(event: React.ChangeEvent<HTMLInputElement>) {
+        if (!isNaN(parseInt(event.target.value))) {
+            setAttemptsRequested(parseInt(event.target.value));
+        }
+    }
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <span>Attempts Left: {attemptsLeft}</span>
+            <Form.Group controlId="formAttemptsRequested">
+                <Form.Label>Enter Requested Attempts (Number):</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={attemptsRequested}
+                    onChange={updateAttemptsLeft}
+                ></Form.Control>
+            </Form.Group>
+            <Button
+                onClick={() => setAttemptsLeft(attemptsLeft - 1)}
+                disabled={attemptsLeft <= 0}
+            >
+                use
+            </Button>
+            <Button
+                onClick={() =>
+                    setAttemptsLeft(attemptsLeft + attemptsRequested)
+                }
+            >
+                gain
+            </Button>
         </div>
     );
 }
